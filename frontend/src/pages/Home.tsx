20b0319b3b61667161a46d5fc50d19c63b7ec66e
Chambo88 <77312049+Chambo88@users.noreply.github.com>
2025-04-27 15:46:38 +1000

    Some of the flashCard viewer is done. Much more to do.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,13 +2,30 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import AnimatedLayout from "@/components/router/AnimatedLayout";
+import LearnDialog from "@/components/learn/learn";
+import {
+  Dialog,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const Home: React.FC = () => {
   return (
     <AnimatedLayout>
-      <div className="dark:bg-blue-1000 relative flex min-h-screen items-end justify-center overflow-hidden bg-[url('src/assets/temp_bg_2.jpg')] bg-cover bg-center">
+      <div
+        className="dark:bg-blue-1000 relative flex min-h-screen
+                   items-end justify-center overflow-hidden
+                   bg-[url('src/assets/temp_bg_6.png')] bg-center"
+      >
+
+
         <div className="mb-16 flex flex-col gap-4 md:flex-row">
-          <Button className="w-56">Learn</Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="w-56">Learn</Button>
+          </DialogTrigger>
+
+          <LearnDialog/>
+        </Dialog>
           <Link to="/edit">
             <Button variant="outline" className="w-56">
               Edit
